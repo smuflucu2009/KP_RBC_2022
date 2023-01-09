91a09efe521285906d0727db8b0b5fb9f992f0b8
MnoorIbra <ibralincah69@gmail.com>
2023-01-09 17:51:17 +0700

    add migration news

---
+++ database/migrations/2023_01_09_053657_galerries.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('galery', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id')->references('id')->on('postingan')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('file');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('galery');
+    }
+};
