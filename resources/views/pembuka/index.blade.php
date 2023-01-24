262902116c2a4a3304ab0a1d84963be383104bf0
Didi Suhardi <didisuhardi00@gmail.com>
2023-01-24 22:13:00 +0700

    Add files via upload
    CSS, landing pages, CSS, dan asset image

--- resources/views/pembuka/index.blade.php
+++ resources/views/pembuka/index.blade.php
@@ -1,15 +1,94 @@
 @extends('boostrap/dasar')
 @section('isi_template')
-<head>
-    <title>Halaman Homepage</title>
-</head>
-<body>
-    <div>
-        <h1>Selamat Datang di Web RBC</h1>
-    </div>
-    <div>
-        <h3>Ini web yang dibuat dengan sepenuh hati</h3>
-    </div>
-</body>
+
+    <head>
+        <title>Ruang Baca Computer Engineering</title>
+    </head>
+
+    <body>
+        <div class="land">
+            <div class="bg">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h1>RUANG BACA <br> COMPUTER ENGINEERING <br></h1>
+                        <button class="download" type="submit" href="/">Mobile App <br> Download</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="panduan">
+            <div >
+                <h3 id="land_header">PANDUAN</h3>
+                <div class="row"></div>
+                <div class="d-flex justify-content-center btn_panduan">
+                    <button class="btn_buku" type="submit"
+                        href="https://docs.google.com/forms/d/e/1FAIpQLSfyZz9He0__48n6PXslXLQrTaWKfjLg6UvWZmAQWGQr-yRfdw/viewform" target="_blank">Peminjaman<br>Buku</button>
+                    <button class="btn_capstone" type="submit" href="http://capstone-ta.ce.undip.ac.id/" target="_blank">Capstone</button>
+                    <button class="btn_kp" type="submit"
+                        href="https://tekkom.ft.undip.ac.id/pendidikan/kerja-praktek" target="_blank">Kerja Praktek</button>
+                </div>
+            </div>
+        <h3 id="land_header">FORMS</h3>
+        <div class="d-flex justify-content-center">
+        <div class="forms">
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfyZz9He0__48n6PXslXLQrTaWKfjLg6UvWZmAQWGQr-yRfdw/viewform" target="_blank">PEMINJAMAN
+                BUKU</a>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSd4YZszxmkytjNM476Lr3GPpXp4bc3hK9v-TZxXdMmSggUYtw/viewform" target="_blank">PENGUNJUNG
+                PERPUS</a>
+            <a href="https://forms.gle/JYTWbyUvCZKD5GJZ8" target="_blank">SUMBANGAN BUKU</a>
+            <a href="https://forms.gle/qfDarxsqVwXLjs727" target="_blank">FEEDBACK US</a>
+        </div>
+        </div>
+        </div>
+
+        <div class="carousel">
+            <div class="cards">
+            </div>
+        </div>
+        <div>
+            <h3 id="land_header">E-RESOURCES</h3>
+        </div>
+        <div class="e-resource">
+            <div id="resource-gap">
+                <a href="https://login.proxy.undip.ac.id/login?url=https://www.sciencedirect.com" target="_blank"><img
+                        src="asset/gambar 6.png"> </a>
+                <a href="https://login.proxy.undip.ac.id/login?url=https://www.scopus.com/home.url" target="_blank"><img
+                        src="asset/gambar 7.png"> </a>
+                <a href="https://login.proxy.undip.ac.id/login?url=https://link.springer.com" target="_blank"><img
+                        src="asset/gambar 8.png"> </a>
+                <a href="https://login.proxy.undip.ac.id/login?url=https://academic.oup.com/" target="_blank"><img
+                        src="asset/gambar 9.png"> </a>
+            </div>
+            <div id="resource-gap">
+                <a href="https://login.proxy.undip.ac.id/login?url=https://www.nature.com" target="_blank"><img
+                        src="asset/gambar 10.png"> </a>
+                <a href="https://login.proxy.undip.ac.id/login?url=https://www.cambridge.org/core" target="_blank"><img
+                        src="asset/gambar 11.png"> </a>
+                <a href="https://login.proxy.undip.ac.id/login?url=https://www.emerald.com/insight" target="_blank"><img
+                        src="asset/gambar 12.png"> </a>
+                <a href="https://login.proxy.undip.ac.id/login?url=https://www.proquest.com" target="_blank"><img
+                        src="asset/gambar 13.png"> </a>
+            </div>
+            <div id="resource-gap">
+                <a href="https://login.proxy.undip.ac.id/login?url=http://www.jstor.org/" target="_blank"> <img
+                        src="asset/gambar 14.png"> </a>
+                <a href="https://login.proxy.undip.ac.id/login?url=http://www.aluka.org/" target="_blank"> <img
+                        src="asset/gambar 15.png"> </a>
+                <a href="https://login.proxy.undip.ac.id/login?url=https://www.clinicalkey.com" target="_blank"> <img
+                        src="asset/gambar 16.png"> </a>
+                <a href="https://login.proxy.undip.ac.id/login?url=https://www.igi-global.com/gateway" target="_blank"> <img
+                        src="asset/gambar 18.png"> </a>
+            </div>
+            <div id="resource-gap">
+                <a href="https://ejournal.undip.ac.id/?t=MTY2ODQ2MDAyMw=="> <img src="asset/gambar 20.png"
+                        href=""> </a>
+                <a href="https://www.ieee.org/"> <img src="asset/gambar 21.png"> </a>
+                <a href="https://login.proxy.undip.ac.id/login?url=https://www.embase.com"> <img
+                        src="asset/gambar 22.png"> </a>
+                <a href="https://login.proxy.undip.ac.id/login?url=https://ascelibrary.org"> <img
+                        src="asset/gambar 19.png"> </a>
+            </div>
+        </div>
+    </body>
 @endsection
-    
---
+++ resources/views/skripsi/index.blade.php
@@ -0,0 +1,14 @@
+@extends('boostrap/dasar')
+@section('isi_template')
+<head>
+    <title>Halaman Skripsi</title>
+</head>
+<body>
+    <div>
+        <h1>Ini Halaman Skripsi</h1>
+    </div>
+    <div>
+        <h2>berisi pusaka TA yang bisa dijadikan referensi untuk menyon... mksdnya menyoba implemnetasikan wkwkkwk</h2>
+    </div>
+</body>
+@endsection
