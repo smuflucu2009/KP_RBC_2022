8c10e49db83125e6e38533261fd48dd7b7fe56b9
M Noor Ibrahim <ibralincah69@gmail.com>
2023-06-02 00:19:46 +0700

    Update skripsi2.php

--- app/Models/skripsi2.php
+++ app/Models/skripsi2.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Skripsi2 extends Model
+class skripsi2 extends Model
 {
     use HasFactory;
 
